a33a19b549a2f1ba2ccdb87e1c6ad751a2eea46a
Lucifer-Jimmy <lucifer.stars@qq.com>
2025-07-28 20:27:24 +0800

    fixbug_1_0728

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.config = window.config || {};
+window.config.lightgallery = {
+  download: false,           // 禁用下载按钮
+  thumbnail: false,          // 关闭底部缩略图
+  // 更多参数见：https://lightgallery.js.org/docs/#settings
+};
